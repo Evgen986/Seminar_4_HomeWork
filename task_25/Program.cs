9b2b0eda162282ef56163a49c99961f23634774b
Evgenyi_Malyutin <Evgen986@mail.run>
2022-10-12 12:14:03 +0300

    HW_Seminar_4

---
+++ task_25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+             3, 5 -> 243 (3⁵)
+             2, 4 -> 16
+*/
+Console.Clear();
+
+Console.Write("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число В: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Число {numberA} в степени {numberB} = {Pow(numberA, numberB)}");
+
+int Pow(int number1, int number2)
+{
+    int result = 1;
+    while (number2 > 0)
+    {
+        result *= number1;
+        number2--;
+    }
+    return result;
+}
---
+++ task_29/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+             1, 2, 5, 7, 19, 2, 4, 9 -> [1, 2, 5, 7, 19, 2, 4, 9 ]
+             6, 1, 33, 2, 93, 15, 424, 0-> [6, 1, 33, 2, 93, 15, 424, 0]
+*/
+Console.Clear();
+int[] array = new int[8];
+
+FillingArray(array);
+PrintArray(array);
+
+
+void FillingArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 1000);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        else
+        {
+            Console.Write(arr[i] + "]");
+        }
+    }
+}
